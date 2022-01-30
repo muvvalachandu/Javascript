046508648035e9fb6085ee16ff07c24427bd1b53
muvvalachandu <muvvala99666@gmail.com>
2022-01-30 11:17:23 +0530

    2nd day 2nd file added

---
+++ p2Assign2.js
@@ -0,0 +1,11 @@
+var dob = 2000;
+var current_year = 2022;
+var age = current_year - dob;
+if(age >= 18){
+
+    console.log('Yes he can vote');
+}
+else{
+
+    console.log('NO,He is not eligible for voting');
+}
