5bcd1c75f35a620ccce115856734a289dc286654
muvvalachandu <muvvala99666@gmail.com>
2022-01-30 11:17:42 +0530

    2nd day 3rd file added

---
+++ p2Assign3.js
@@ -0,0 +1,32 @@
+var vowel = 'b';
+
+if(vowel == 'a' || vowel == 'A')
+    {
+      console.log('it is vowel');
+
+    }
+    else if (vowel == 'e' || vowel == 'E')
+    {
+    
+        console.log('it is vowel');
+    }
+    else if(vowel == 'i' || vowel == 'I')
+    {
+
+        console.log('it is vowel');
+    }
+    else if(vowel =='o' || vowel == 'O')
+    {
+
+        console.log('it is vowel');
+    }
+    else if(vowel =='u' || vowel == 'U')
+    {
+
+        console.log('it is vowel');
+    
+    }
+      else
+      {
+          console.log("it is consonent");
+      }
